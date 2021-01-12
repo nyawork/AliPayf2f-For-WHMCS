fde60238ec28672b611c83a9444b4f6b188bb7b0
Candinya <dev@candinya.com>
2021-01-12 21:55:54 +0800

    update: expose all

--- modules/gateways/class/alipay_full/config_gen.php
+++ modules/gateways/class/alipay_full/config_gen.php
@@ -24,44 +24,21 @@ class alipayfull_config{
                 ]
             ];
         } else {
-            if ($type->value === "1" || $type->value === "2") {
                 $extra_config = [
                     "seller_email" => ["FriendlyName" => "卖家支付宝帐户", "Type" => "text", "Size" => "50","Description" => "需要申请支付宝商家集成", ],
                     "partnerID" => ["FriendlyName" => "合作伙伴ID", "Type" => "text", "Size" => "50","Description" => "到你的支付宝商家后台查找", ],
                     "security_code" => ["FriendlyName" => "安全检验码 (MD5)", "Type" => "text", "Size" => "50", "Description" => "同上",],
+                        "app_id" => ["FriendlyName" => "应用ID (APPID)", "Type" => "text", "Size" => "60"],
+                        "alipay_key" => ["FriendlyName" => "支付宝公钥", "Type" => "textarea",  'Rows' => '10', 'Cols' => '60' , 'Value'=>'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlagtk1bbiFdKw/ADO8y7ra6wbHyPOwSY5V9sir/rPWd/7H+vw8RAHadJWM0HExZRSU+zBhgbGV4uLVbSoBTSemuubBdeANHE2jeE8iOit8oJdSrqEr64Ls7YtUsHYCd1LRwt2j+guG+LTigpPOvKXgD79GRCy9Nx2WOVcHda6ZaO4WP1+M9oPogLgnJBO8Dwifv22pVgIruzEn/LRYoDeUNdJTHm35we1XECXnBsInj9rVOv+MPgByAyqyD70qFNfO2N2Q2RBxvJVwJsjDZgEDDkeaxFmc/qHmZaJSfZwcryst2aWyAdkvFUtaD9QmBdt30fZ796OekwYQITCLwt7QIDAQAB'],
+                        "rsa_key" => ["FriendlyName" => "RSA2(SHA256) 私钥", "Type" => "textarea",  'Rows' => '10', 'Cols' => '60',"Description" => '您可能需要 :<br/><a type="button" class="btn btn-primary" href="https://os.alipayobjects.com/download/secret_key_tools_RSA256_win.zip" target="_blank"><span class="glyphicon glyphicon-new-window"></span> RSA2(SHA256) 生成器下载</a> <a type="button" class="btn btn-primary" href="https://doc.open.alipay.com/docs/doc.htm?articleId=106130&docType=1" target="_blank"> <span class="glyphicon glyphicon-new-window"></span> OpenSSL生成教程</a><br/>  生成器私钥文件名 : rsa_private_key.pem 公钥文件名 : rsa_public_key.pem<br/>将私钥文件内容<br/>使用<span style="color:red">非Windows记事本打开</span> , 并将里面内容复制到上面文本框中<br/>公钥则请到'." <a href='https://open.alipay.com/platform/keyManage.htm' target='_blank'><span class='glyphicon glyphicon-new-window'></span> 商家支付宝 开放平台</a> 绑定", ],
                     "notice" => [
                     'FriendlyName' => '',
                     'Type' => 'dropdown',
                     'Options' => [
-                        '1' => "</option></select><div class='alert alert-info' role='alert' id='alipay_full_notice' style='margin-bottom: 0px;'>以上信息均可以在 <a href='https://openhome.alipay.com/platform/keyManage.htm?keyType=partner' target='_blank'><span class='glyphicon glyphicon-new-window'></span> 商家支付宝 开放平台</a> 找到 。 请确保已经在支付宝签约 即时到账 (手机则需要手机网站支付) 必需合约</div><script>$('#alipay_full_notice').prev().hide();</script><select style='display:none'>",
+                        '1' => "</option></select><div class='alert alert-info' role='alert' id='alipay_full_notice' style='margin-bottom: 0px;'>以上信息均可以在 <a href='https://openhome.alipay.com/platform/keyManage.htm?keyType=partner' target='_blank'><span class='glyphicon glyphicon-new-window'></span> 商家支付宝 开放平台</a> 找到 。 如果您需要使用 即时到账 (手机则需要手机网站支付) 功能，您需要填写 卖家支付宝帐户 + 合作伙伴ID + 安全检验码 (MD5)。开启手机网站支付功能前需要开启对应的功能, 否则未申请手机端将不会显示支付界面(显示未签约或其他错误页面)。	</div><div class='alert alert-info' role='alert' id='alipay_full_moblie' style='margin-bottom: 0px;'>如果您需要使用 当面付 功能，您需要填写 应用ID (APPID) + 支付宝公钥 + RSA2(SHA256) 私钥，上三项可以忽略。 </div><script>$('#alipay_full_notice').prev().hide();</script><select style='display:none'>",
                         ],
                     ]
                 ];
-                if ($type->value === "2") {
-                    $extra_config["extra_notice"] =  [
-                        'FriendlyName' => '',
-                        'Type' => 'dropdown',
-                        'Options' => [
-                            '1' => "</option></select><div class='alert alert-info' role='alert' id='alipay_full_moblie' style='margin-bottom: 0px;'>请确保已经申请支付宝手机网站支付功能 , 否则未申请手机端将不会显示支付界面(显示未签约或其他错误页面)</div><script>$('#alipay_full_moblie').prev().hide();</script><select style='display:none'>",
-                            ],
-                        ];
-                }
-            } elseif ($type->value === "3") {
-                    $extra_config = [
-                        "app_id" => ["FriendlyName" => "应用ID (APPID)", "Type" => "text", "Size" => "60"],
-                        "alipay_key" => ["FriendlyName" => "支付宝公钥", "Type" => "textarea",  'Rows' => '10', 'Cols' => '60' , 'Value'=>'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAlagtk1bbiFdKw/ADO8y7ra6wbHyPOwSY5V9sir/rPWd/7H+vw8RAHadJWM0HExZRSU+zBhgbGV4uLVbSoBTSemuubBdeANHE2jeE8iOit8oJdSrqEr64Ls7YtUsHYCd1LRwt2j+guG+LTigpPOvKXgD79GRCy9Nx2WOVcHda6ZaO4WP1+M9oPogLgnJBO8Dwifv22pVgIruzEn/LRYoDeUNdJTHm35we1XECXnBsInj9rVOv+MPgByAyqyD70qFNfO2N2Q2RBxvJVwJsjDZgEDDkeaxFmc/qHmZaJSfZwcryst2aWyAdkvFUtaD9QmBdt30fZ796OekwYQITCLwt7QIDAQAB'],
-                        "rsa_key" => ["FriendlyName" => "RSA2(SHA256) 私钥", "Type" => "textarea",  'Rows' => '10', 'Cols' => '60',"Description" => '您可能需要 :<br/><a type="button" class="btn btn-primary" href="https://os.alipayobjects.com/download/secret_key_tools_RSA256_win.zip" target="_blank"><span class="glyphicon glyphicon-new-window"></span> RSA2(SHA256) 生成器下载</a> <a type="button" class="btn btn-primary" href="https://doc.open.alipay.com/docs/doc.htm?articleId=106130&docType=1" target="_blank"> <span class="glyphicon glyphicon-new-window"></span> OpenSSL生成教程</a><br/>  生成器私钥文件名 : rsa_private_key.pem 公钥文件名 : rsa_public_key.pem<br/>将私钥文件内容<br/>使用<span style="color:red">非Windows记事本打开</span> , 并将里面内容复制到上面文本框中<br/>公钥则请到'." <a href='https://open.alipay.com/platform/keyManage.htm' target='_blank'><span class='glyphicon glyphicon-new-window'></span> 商家支付宝 开放平台</a> 绑定", ],
-                        "notice" => [
-                        'FriendlyName' => '',
-                        'Type' => 'dropdown',
-                        'Options' => [
-                            '1' => "</option></select><div class='alert alert-info' role='alert' id='alipay_full_notice' style='margin-bottom: 0px;'>以上信息均可以在 <a href='https://open.alipay.com/platform/keyManage.htm' target='_blank'><span class='glyphicon glyphicon-new-window'></span> 商家支付宝 开放平台</a> 找到 。 请确保已经在支付宝签约 当面付 必需合约</div><script>$('#alipay_full_notice').prev().hide();</script><select style='display:none'>",
-                            ],
-                        ]
-                    ];
-            } else {
-                $extra_config = [];
-            }
         }
         $base_config = [
             "FriendlyName" => ['Type' => 'System','Value' => '支付宝扫码支付',],
@@ -95,7 +72,7 @@ class alipayfull_config{
             'FriendlyName' => '',
             'Type' => 'dropdown',
             'Options' => [
-                '1' => "</option></select><div class='alert alert-success' role='alert' id='alipay_full_author' style='margin-bottom: 0px;'>Powered by <a href='https://www.idc.moe/' target='_blank'><span class='glyphicon glyphicon-new-window'></span>ITXE Studio</a>.<br/><span class='glyphicon glyphicon-ok'></span> 支持 WHMCS 5.x-7.x , 当前WHMCS 版本 ".$CONFIG["Version"]."<br/><span class='glyphicon glyphicon-ok'></span> 仅支持 PHP 5.4 以上的环境 , 当前PHP版本 ".phpversion()."</div><script>$('#alipay_full_author').prev().hide();</script><style>* {font-family: Microsoft YaHei Light , Microsoft YaHei}</style><select style='display:none'>",
+                '1' => "</option></select><div class='alert alert-success' role='alert' id='alipay_full_author' style='margin-bottom: 0px;'>Modified by  <a href='https://nyawork.com/' target='_blank'><span class='glyphicon glyphicon-new-window'></span>Nyawork</a> from <a href='https://www.idc.moe/' target='_blank'><span class='glyphicon glyphicon-new-window'></span>ITXE Studio</a>.<br/><span class='glyphicon glyphicon-ok'></span> 支持 WHMCS 5.x-8.x , 当前WHMCS 版本 ".$CONFIG["Version"]."<br/><span class='glyphicon glyphicon-ok'></span> 仅支持 PHP 5.4 以上的环境 , 当前PHP版本 ".phpversion()."</div><script>$('#alipay_full_author').prev().hide();</script><style>* {font-family: Microsoft YaHei Light , Microsoft YaHei}</style><select style='display:none'>",
             ],
         ];
         return $config;
